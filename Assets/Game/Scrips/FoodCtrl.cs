e8c0470139c9ab420e7c5cf8246dfc1791bb896c
Oliver Perez <oliver.a.perez.c@gmail.com>
2016-02-06 17:56:57 -0500

    Add project base files

---
+++ Assets/Game/Scrips/FoodCtrl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start  () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+}
