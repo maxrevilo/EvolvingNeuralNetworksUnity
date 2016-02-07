86558548cb6eed3fb34601224cd3e9df29ff9256
Oliver Perez <oliver.a.perez.c@gmail.com>
2016-02-07 15:50:53 -0500

    Adds initial prefab pool spawning in Genetic Gravity Scene

--- Assets/Game/Scrips/GeneticGravityManager.cs
+++ Assets/Game/Scrips/GeneticGravityManager.cs
@@ -1,170 +1,111 @@
 using UnityEngine;
 using System;
-using System.Collections.Generic;
+using ANNs;
 
 public class GeneticGravityManager : SceneManager
-{/*
+{
     [SerializeField]
-    private BoxCollider2D stageArea;
+    private BoxCollider stageArea;
     [SerializeField]
-    private GameObject critterPrefab;
+    private GameObject boxPrefab;
     [SerializeField]
-    private GameObject foodPrefab;
+    private Transform targetDestination;
     [SerializeField]
-    private int crittersAmount = 10;
-    [SerializeField]
-    private int foodAmount = 40;
+    private int boxesAmount = 100;
 
-    private GameObject crittersParent;
-    private GameObject consumablesParent;
+    private GameObject boxesParent;
 
-    public GameObject[] critters { get; private set; }
-    public GameObject[] foods { get; private set; }
-
-    private int crittersAlive = 0;
+    public GameObject[] boxes { get; private set; }
 
     protected override void Start()
     {
         if (stageArea == null) throw new Exception("stageArea is not set");
-        if (critterPrefab == null) throw new Exception("critterPrefab is not set");
-        if (foodPrefab == null) throw new Exception("foodPrefab is not set");
+        if (boxPrefab == null) throw new Exception("boxPrefab is not set");
 
         stageArea.isTrigger = true;
 
-        SpawnFood();
-        SpawnCritters();
+        SpawnBoxes();
 
         base.Start();
     }
 
-    void SpawnCritters()
+    void SpawnBoxes()
     {
-        crittersParent = new GameObject("Critters");
-        crittersParent.transform.SetParent(transform);
+        boxesParent = new GameObject("Boxes");
+        boxesParent.transform.SetParent(transform);
 
-        critters = new GameObject[crittersAmount];
+        boxes = new GameObject[boxesAmount];
 
-        for (int i = 0; i < crittersAmount; i++)
-        {
-            GameObject critter = (GameObject)Instantiate(critterPrefab);
-            critter.transform.SetParent(crittersParent.transform);
-            critters[i] = critter;
-
-            CritterSensors critterSensors = critter.GetComponent<CritterSensors>();
-            critterSensors.scene = this;
-
-            CritterCtrl critterCtrl = critter.GetComponent<CritterCtrl>();
-            critterCtrl.OnDied += CritteriDed;
-        }
-    }
-
-    void ResetCritters()
-    {
         Bounds stageBounds = stageArea.bounds;
 
-        foreach (GameObject critter in critters)
+        for (int i = 0; i < boxesAmount; i++)
         {
+
+            GameObject box = (GameObject)Instantiate(boxPrefab);
+            box.transform.SetParent(boxesParent.transform);
+            boxes[i] = box;
+
             Vector3 position = new Vector3(
                 UnityEngine.Random.Range(stageBounds.min.x, stageBounds.max.x),
                 UnityEngine.Random.Range(stageBounds.min.y, stageBounds.max.y),
-                0
+                UnityEngine.Random.Range(stageBounds.min.z, stageBounds.max.z)
             );
 
-            Quaternion rotation = Quaternion.AngleAxis(UnityEngine.Random.Range(0, 360), Vector3.forward);
-
-            critter.transform.position = position;
-            critter.transform.rotation = rotation;
-
-            CritterCtrl critterCtrl = critter.GetComponent<CritterCtrl>();
-            critterCtrl.Initialize();
-
-            crittersAlive++;
+            box.transform.position = position;
         }
     }
 
-    void SpawnFood()
+    void ResetBoxes()
     {
-        consumablesParent = new GameObject("Consumables");
-        consumablesParent.transform.SetParent(transform);
-
-        foods = new GameObject[foodAmount];
-
-        for (int i = 0; i < foodAmount; i++)
-        {
-            GameObject food = (GameObject)Instantiate(foodPrefab);
-            food.transform.SetParent(consumablesParent.transform);
-            foods[i] = food;
-        }
-    }
-
-    void ResetFood()
-    {
-        Bounds stageBounds = stageArea.bounds;
-
-        foreach (GameObject food in foods)
-        {
-            Vector3 position = new Vector3(
-                UnityEngine.Random.Range(stageBounds.min.x, stageBounds.max.x),
-                UnityEngine.Random.Range(stageBounds.min.y, stageBounds.max.y),
-                0
+        foreach (GameObject box in boxes) {
+               
+            Quaternion rotation = Quaternion.AngleAxis(
+                UnityEngine.Random.Range(0, 360),
+                Vector3.forward
             );
+            box.transform.rotation = rotation;
 
-            Quaternion rotation = Quaternion.AngleAxis(UnityEngine.Random.Range(0, 360), Vector3.forward);
-
-            food.transform.position = position;
-            food.transform.rotation = rotation;
-
-            FoodCtrl foodCtrl = food.GetComponent<FoodCtrl>();
-            foodCtrl.Initialize();
-        }
-    }
-
-    void CritteriDed(GameObject critter)
-    {
-        crittersAlive--;
-
-        if (crittersAlive <= 0)
-        {
-            EndGeneration();
+            Rigidbody boxBody = box.GetComponent<Rigidbody>();
+            if(boxBody)
+            {
+                boxBody.angularVelocity = new Vector3(
+                UnityEngine.Random.Range(-0.1f, 0.1f),
+                UnityEngine.Random.Range(-0.1f, 0.1f),
+                UnityEngine.Random.Range(-0.1f, 0.1f)
+            );
+            }
         }
     }
 
     protected override void StartGeneration()
     {
-        crittersAlive = 0;
-
-        ResetFood();
-        ResetCritters();
-
+        ResetBoxes();
+        /*
         if (generations.Count > 0)
         {
             Generation prevGeneration = generations.Last.Value;
-            float[][][] offspring = prevGeneration.GenerateOffspring(crittersAmount);
+            float[][][] offspring = prevGeneration.GenerateOffspring(boxesAmount);
 
             int i = 0;
             foreach (float[][] genes in offspring)
             {
-                GameObject critter = critters[i];
-                CritterANNControl ann = critter.GetComponent<CritterANNControl>();
-                ann.neuralNetwork.SetWeights(genes);
+                GameObject box = boxes[i];
                 i++;
             }
         }
-
+        */
         base.StartGeneration();
     }
 
     protected override void EndGeneration()
     {
         Generation currentGeneration = generations.Last.Value;
-
-        foreach (GameObject critter in critters)
+        /*
+        foreach (GameObject box in boxes)
         {
-            CritterCtrl ctrl = critter.GetComponent<CritterCtrl>();
-            CritterANNControl ann = critter.GetComponent<CritterANNControl>();
-            currentGeneration.AddPhenotype(ann.neuralNetwork.ws, ctrl.LifeSpan() - ctrl.naturalLifeSpan());
-        }
+            currentGeneration.AddPhenotype(ann.neuralNetwork.ws, 0);
+        }*/
 
         base.EndGeneration();
-    }*/
+    }
 }
