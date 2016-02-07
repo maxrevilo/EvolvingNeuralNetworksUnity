c2391fce3a5407cc729f7f3bb6853f0d7e43baee
Oliver Perez <oliver.a.perez.c@gmail.com>
2016-02-07 17:07:52 -0500

    Adds basic informative GUI

--- Assets/Game/Scrips/SceneManager.cs
+++ Assets/Game/Scrips/SceneManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Collections.Generic;
 using ANNs;
@@ -7,7 +8,15 @@ public class SceneManager : BaseBehaviour
 {
     public int generationNumber { get; protected set; }
 
+    [SerializeField]
+    private Text generationTxt;
+    [SerializeField]
+    private Text infoTxt;
+
     protected LinkedList<Generation> generations;
+
+    protected float bestFitness = 0;
+    protected float avgFitness = 0;
 
     protected virtual void Start()
     {
@@ -19,6 +28,12 @@ public class SceneManager : BaseBehaviour
     protected virtual void StartGeneration()
     {
         generationNumber++;
+
+        if(generationTxt != null)
+        {
+            generationTxt.text = "GEN " + generationNumber;
+        }
+
         Debug.Log("New generation " + generationNumber);
         Generation currentGeneration = new Generation("G" + generationNumber);
         generations.AddLast(currentGeneration);
@@ -28,6 +43,20 @@ public class SceneManager : BaseBehaviour
     {
         Generation currentGeneration = generations.Last.Value;
         Debug.Log(String.Format("Generation ended, fitness {0}s", currentGeneration.BestFitness() ));
+
+        avgFitness = (avgFitness * (generations.Count - 1) + currentGeneration.BestFitness()) / generations.Count;
+
+        if (bestFitness < currentGeneration.BestFitness()) bestFitness = currentGeneration.BestFitness();
+
+        if (infoTxt != null)
+        {
+            infoTxt.text = String.Format(
+                "Last Fit {0:0.00}s, Avg Fit {1:0.00}s Best Fit {2:0.00}s",
+                currentGeneration.BestFitness(),
+                avgFitness,
+                bestFitness
+            );
+        }
         
         Invoke("StartGeneration", 3);
     }
